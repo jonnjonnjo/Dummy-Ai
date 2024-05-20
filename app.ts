f8481158a417c35fe0dd95ecf4bb96f191cf31a3
jon <zhenagajo234@gmail.com>
2024-05-20 11:27:05 +0700

    edit : readme

---
+++ app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+const port = 9013;;
+
+app.use(express.json())
+
+function getRandom0Or1(): number {
+    return Math.round(Math.random());
+  }
+
+app.post('/classify',(req,res)=>{
+    const result = getRandom0Or1();
+    res.json({result})
+})
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+})
